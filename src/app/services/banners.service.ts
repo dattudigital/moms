4742f327b272a36d163e2d0997c88840feeeb367
dattudigital <dattu@digitalrupay.com>
2019-01-12 17:59:48 +0530

    banners validations

---
+++ src/app/services/banners.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http} from '@angular/http';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BannersService {
+
+  constructor(private http:Http) { }
+  
+  public saveBanners(data: any) {
+    return this.http.post(environment.host + 'promotions',data);
+  }
+  
+  public listBanners(){
+    return this.http.get( environment.host + 'promotions');
+  }
+
+}
--- src/app/views/banner-user-activities/banner.component.ts
+++ src/app/views/banner-user-activities/banner.component.ts
@@ -1,9 +1,9 @@
 import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
 import { AlertConfig } from 'ngx-bootstrap/alert';
-import { BeautyTipsService } from '../../services/beauty-tips.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastyService, ToastOptions } from 'ng2-toasty';
+import { BannersService } from '../../services/banners.service';
 
 @Component({
     templateUrl: 'banner.component.html'
@@ -32,16 +32,8 @@ export class BannerComponent implements OnInit {
         theme: 'default'
     };
 
-    banner: any = {
-        'deal_id': null,
-        'deal_name': '',
-        'deal_short_desc': '',
-        'deal_long_desc': '',
-        'deal_type': '',
-        'deal_image': '',
-        'profile_name': '',
-        'deal_status': ''
-    }
+
+    banners: any = [];
     bannerForm: FormGroup;
     dealAdd: any = [];
     submitted = false;
@@ -54,10 +46,137 @@ export class BannerComponent implements OnInit {
     userimagePreview: any;
     userImage: string;
 
-    constructor() {
+    banner: any = {
+        'promotion_id': null,
+        'promotion_name': '',
+        'promotion_type': '',
+        'promotion_img': '',
+        'promotion_description': '',
+        'promotion_for': '',
+        'rec_status': ''
+    }
+
+    constructor(private service: BannersService, private toastyService: ToastyService, private formBuilder: FormBuilder, private cdr: ChangeDetectorRef) {
 
     }
-    ngOnInit() {
+    ngAfterViewChecked() {
+        this.cdr.detectChanges();
+      }
 
+    ngOnInit() {
+        this.service.listBanners().subscribe(res => {
+            if (res.json().status == true) {
+                this.banners = res.json().result;
+            }
+        })
+        this.bannerForm = this.formBuilder.group({
+            Name: ['', Validators.required],
+            for: ['', Validators.required],
+            desc: ['', Validators.required]
+        });
+    }
+
+    get f() { return this.bannerForm.controls; }
+
+    addOrUpdateBanners() {
+        console.log("came")
+        this.submitted = true;
+        if (this.bannerForm.invalid) {
+            return;
+        }
+
+        if (!this.banner.rec_status) {
+            this.banner.rec_status = '1'
+        }
+        if (!this.banner.promotion_id) {
+            this.banner.promotion_id = null;
+        }
+        var data = {
+            promotion_id: this.banner.promotion_id,
+            promotion_name: this.banner.promotion_name,
+            promotion_type: this.banner.promotion_type,
+            promotion_img: this.banner.promotion_img,
+            promotion_description: this.banner.promotion_description,
+            promotion_for: this.banner.promotion_for,
+            rec_status: this.banner.rec_status
+        }
+        console.log(data);
+        let modelClose = document.getElementById("CloseButton");
+        this.service.saveBanners(data).subscribe(res => {
+            modelClose.click();
+            if (res.json().status == true) {
+                if (!this.banner.deal_id) {
+                    this.banners.push(res.json().result)
+                } else {
+                    let _index = ((this.currentPage - 1) * 3) + this.banner["index"]
+                    console.log(_index)
+                    if (this.banner.rec_status == '0') {
+                        this.banners.splice(_index, 1);
+                    } else {
+                        this.banners[_index] = res.json().result;
+                    }
+                }
+                this.toastyService.success(this.toastOptionsSuccess);
+            } else {
+                this.toastyService.error(this.toastOptionsError);
+            }
+        })
+    }
+
+    getFileDetails(event) {
+        var files = event.target.files;
+        var file = files[0];
+        if (files && file) {
+            var reader = new FileReader();
+            reader.onload = this._handleReaderLoaded.bind(this);
+            reader.readAsBinaryString(file);
+        }
+        if (event.target.files && event.target.files[0]) {
+            var reader = new FileReader();
+            reader.readAsDataURL(event.target.files[0]);
+            this.banner.promotion_img = file.name;
+            reader.onload = (event) => {
+                this.userimagePreview = event.target;
+            }
+        }
+    }
+
+    _handleReaderLoaded(readerEvt) {
+        var binaryString = readerEvt.target.result;
+        this.banner.promotion_img = btoa(binaryString);
+        this.isShowOriginalImg = true;
+        if (this.banner.promotion_id) {
+            this.isShowOriginalImg = true;
+        }
+    }
+
+    editBanner(data, index) {
+        this.copiedRow = Object.assign({}, data);
+        this.banner = data;
+        console.log(this.banner)
+        this.banner["index"] = index;
+    }
+
+    backupData() {
+        console.log('@@@@')
+        let _index = ((this.currentPage - 1) * 3) + this.banner["index"]
+        console.log(_index);
+        this.banners[_index] = this.copiedRow;
+    }
+
+    deleteBanner(data, index) {
+        this.deleteRecord = data;
+        this.deleteRecord["index"] = index
+        console.log(this.deleteRecord)
+    }
+
+    removeFields() {
+        this.banner.promotion_id = '';
+        this.banner.promotion_name = '';
+        this.banner.promotion_type = '';
+        this.banner.promotion_img = '';
+        this.banner.promotion_description = '';
+        this.banner.promotion_for = '';
+        this.banner.rec_status = '';
     }
 }
