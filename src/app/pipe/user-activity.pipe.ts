dc91b28a415473b8745802b7b8c89def5f8a3361
rajanipos <rajani@digitalrupay.com>
2019-03-27 15:36:42 +0530

    user-activity filter

--- src/app/pipe/user-activity.pipe.ts
+++ src/app/pipe/user-activity.pipe.ts
@@ -8,12 +8,12 @@ export class UserActivityPipe implements PipeTransform {
   transform(value: any, args?: any): any {
     console.log(value)
     value.forEach(element => {
-      console.log(element)
-      console.log(element.user_activity_status)
       if (element.user_activity_status == 1) {
         element.user_activity_status = "Active";
       }
-      console.log(element.user_activity_status)
+      if (element.user_activity_status == 0) {
+        element.user_activity_status = "InActive";
+      }
     });
     return value;
   }
--- src/app/services/toasty-message.service.ts
+++ src/app/services/toasty-message.service.ts
@@ -36,7 +36,8 @@ export class ToastyMessageService {
   }
 
   errorToast(val: any) {
-    this.toastOptionsSuccess.msg = val;
+    console.log(val)
+    this.toastOptionsError.msg = val;
     this.toastyService.error(this.toastOptionsError);
   }
 
